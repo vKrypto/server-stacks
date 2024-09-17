eaff21ddb6fa3bf371df86cbbda4eb4994398134
ashutosh <vkrypto@gmail.com>
2024-09-18 00:25:36 +0530

    auto syncing..

--- tech-stack-architectures/app_stack/docker-compose/apps/express/app.js
+++ tech-stack-architectures/app_stack/docker-compose/apps/express/app.js
@@ -3,6 +3,23 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const promBundle = require("express-prom-bundle");
+
+// Add the options to the prometheus middleware most option are for http_request_duration_seconds histogram metric
+const metricsMiddleware = promBundle({
+    includeMethod: true, 
+    includePath: true, 
+    includeStatusCode: true, 
+    includeUp: true,
+    customLabels: {project_name: 'hello_world', project_type: 'test_metrics_labels'},
+    promClient: {
+        collectDefaultMetrics: {
+        }
+      }
+});
+// add the prometheus middleware to all routes
+app.use(metricsMiddleware)
+
 app.get('/', (req, res) => {
   res.send('Hello from the Express app!');
 });
--- tech-stack-architectures/app_stack/docker-compose/apps/fastapi/app/main.py
+++ tech-stack-architectures/app_stack/docker-compose/apps/fastapi/app/main.py
@@ -1,9 +1,14 @@
 # app/main.py
 
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
+Instrumentator().instrument(app).expose(
+    app, endpoint="/prometheus/metrics", should_gzip=False
+)
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI!"}
+    return {"ping": "pong!"}
