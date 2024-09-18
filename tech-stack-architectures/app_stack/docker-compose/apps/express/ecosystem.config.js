4439d652dad0defaa951ca628aee9c301b28c687
ashutosh <vkrypto@gmail.com>
2024-09-19 01:14:47 +0530

    auto syncing..

--- tech-stack-architectures/app_stack/docker-compose/apps/express/ecosystem.config.js
+++ tech-stack-architectures/app_stack/docker-compose/apps/express/ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps: [
       {
         name: "express-app",  // Name of the application
+        cwd: "demo_app",
         script: "./app.js",   // The entry point of your app
         instances: 1,         // Number of instances (1 for single instance, or use 'max' for all CPU cores)
         autorestart: true,    // Auto-restart if the app crashes
--- tech-stack-architectures/app_stack/docker-compose/apps/nextjs/pm2.config.js
+++ tech-stack-architectures/app_stack/docker-compose/apps/nextjs/pm2.config.js
@@ -3,9 +3,13 @@
 module.exports = {
     apps: [{
       name: "nextjs-app",
+      cwd: "demo_app",
       script: "npm",
       args: "start",
-      instances: "max",  // Utilize all CPU cores
+      instances: 1,         // Number of instances (1 for single instance, or use 'max' for all CPU cores)
+      autorestart: true,    // Auto-restart if the app crashes
+      watch: false,         // Disable file watching for changes
+      max_memory_restart: "300M", // Restart if memory usage exceeds 300MB
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
