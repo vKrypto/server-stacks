bf41f2e7a59f752bba35d4b6e5fc39d7fd2c6366
ashutosh <vkrypto@gmail.com>
2024-09-18 22:47:05 +0530

    auto syncing..

--- tech-stack-architectures/app_stack/docker-compose/apps/express/app.js
+++ tech-stack-architectures/app_stack/docker-compose/apps/express/app.js
@@ -7,6 +7,7 @@ const promBundle = require("express-prom-bundle");
 
 // Add the options to the prometheus middleware most option are for http_request_duration_seconds histogram metric
 const metricsMiddleware = promBundle({
+    metricsPath: "/prometheus/metrics",
     includeMethod: true, 
     includePath: true, 
     includeStatusCode: true, 
---
+++ tech-stack-architectures/app_stack/docker-compose/apps/express/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+      {
+        name: "express-app",  // Name of the application
+        script: "./app.js",   // The entry point of your app
+        instances: 1,         // Number of instances (1 for single instance, or use 'max' for all CPU cores)
+        autorestart: true,    // Auto-restart if the app crashes
+        watch: false,         // Disable file watching for changes
+        max_memory_restart: "300M", // Restart if memory usage exceeds 300MB
+        env: {
+          NODE_ENV: "development",
+          PORT: 3000           // Environment variables for development
+        },
+        env_production: {
+          NODE_ENV: "production",
+          PORT: 3000           // Environment variables for production
+        }
+      }
+    ]
+  };
+  
