ede21b823a34dd00b2da4e5d1805516da0a13143
ashutosh <vkrypto@gmail.com>
2024-09-29 13:18:14 +0530

    auto syncing..

--- tech-stack-architectures/app_stack/docker-compose/conf/slave_init.sh
+++ tech-stack-architectures/app_stack/docker-compose/conf/slave_init.sh
@@ -5,6 +5,14 @@ set -e
 MASTER_HOST="${MASTER_HOST:-master_db}"
 MASTER_PORT="${MASTER_PORT:-5432}"
 
+# Create the standby.signal file to enable replication
+touch /var/lib/postgresql/data/standby.signal
+
+# Optionally set permissions for the standby.signal file
+chown postgres:postgres /var/lib/postgresql/data/standby.signal
+chmod 600 /var/lib/postgresql/data/standby.signal
+
+# strart replica-sync
 bash -c "
 until pg_basebackup --pgdata=/var/lib/postgresql/data -R --slot=replication_slot_${CONTAINER_NAME} --host=${MASTER_HOST} --port=${MASTER_PORT}
 do
